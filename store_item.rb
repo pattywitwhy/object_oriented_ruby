03ad22c02f855259b6b0ad9d40f28e734eca08ab
pattywitwhy <pattyxhwang@gmail.com>
2019-01-08 18:57:20 -0600

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+# item_1 = ["white", 2.99, "bread"]
+# item_2 = ["red", 5.99, "meat"]
+# item_3 = ["yellow", 1.99, "chip"]
+
+item_1 = {:color => "white", :price => 2.99, :type => "bread"} #Ruby
+item_2 = {:color => "red", :price => 5.99, :type => "meat"}
+item_3 = {:color => "yellow", :price => 1.99, :type => "chip"}
